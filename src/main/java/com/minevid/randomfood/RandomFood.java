083afbcf17ba592294af7a17888b1e6122a25fd0
Reynout De Wolf <reynout.dewolf.y0468@student.hogent.be>
2017-03-07 12:28:05 +0100

    Beginning

---
+++ src/main/java/com/minevid/randomfood/RandomFood.java
@@ -0,0 +1,7 @@
+package com.minevid.randomfood;
+
+/**
+ * Created by Reynout on 7/03/2017.
+ */
+public class RandomFood {
+}
---
+++ src/main/java/com/minevid/randomfood/references/References.java
@@ -0,0 +1,14 @@
+package com.minevid.randomfood.references;
+
+/**
+ * Created by Reynout on 7/03/2017.
+ */
+public class References {
+
+
+    public static final String MOD_ID = "randomfood";
+    public static final String MOD_NAME = "Random Food";
+    public static final String VERSION = "1.10.2-1.0";
+
+
+}
